864384823ac9157a6942b2a846237228a37adc00
Mohit Prathipati <prathipati.mohit@gmail.com>
2024-05-26 14:28:37 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,43 @@
+date = input("Enter the date in mm/dd/yy format: ")
+month = date[0:2]
+day = date[3:5]
+year = date[6:8]
+
+if date[0:2] == "01":
+    print("January " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "02":
+    print("February " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "03":
+    print("March " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "04":
+    print("April " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "05":
+    print("May " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "06":
+    print("June " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "07":
+    print("July " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "08":
+    print("August " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "09":
+    print("September " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "10":
+    print("October " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "11":
+    print("November " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+elif date[0:2] == "12":
+    print("December " + day + ", " + year)
+    print(day + "/" + month + "/" + year)
+
+
